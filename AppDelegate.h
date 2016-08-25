428e219e40da083d301ae747316be91caeb8ec2a
XiaLiancheng <474358032@qq.com>
2016-08-25 10:59:50 +0800

    Test3

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test3
+//
+//  Created by apple on 16/8/25.
+//  Copyright © 2016年 Haier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
